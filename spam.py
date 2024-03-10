7e3f0105fcb302712cf912cc85311039c25c2e16
Ishaisprolityeral <109461053+Ishaisprolityeral@users.noreply.github.com>
2024-03-10 21:53:55 +0800

    Update spam.py

--- spam.py
+++ spam.py
@@ -12,5 +12,5 @@ for i in range(1000):
     payload = {
         'content': i
     }
-    r = requests.post('OTQ1MjQ2MTMyMzg3MzM2MjIy.GXhdLM.qUiPkNOCEzGFT4-wKVTe3R5nn3PLO6ZXNo1pFc', data=payload, headers=header)
+    r = requests.post('', data=payload, headers=header)
   
